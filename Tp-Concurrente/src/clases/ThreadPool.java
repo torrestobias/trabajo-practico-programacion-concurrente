e88fef2a6658712d761c3a805e065ecc9de6a1e0
MatiasCar <matiascardozo2795@gmail.com>
2020-10-30 19:05:56 -0300

    agregado constructor ThreadPool

--- Tp-Concurrente/src/clases/ThreadPool.java
+++ Tp-Concurrente/src/clases/ThreadPool.java
@@ -1,4 +1,12 @@
 package clases;
 
 public class ThreadPool {
+
+    private int cantWorkers;
+    private Buffer buffer;
+
+    public ThreadPool(int cantWorkers, Buffer buffer){
+        this.cantWorkers = cantWorkers;
+        this.buffer = buffer;
+    }
 }
