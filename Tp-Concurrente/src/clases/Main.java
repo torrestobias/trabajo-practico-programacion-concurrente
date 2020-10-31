f863c4d6b1b3645bf1b0131c0db4896c0b7d7de3
MatiasCar <matiascardozo2795@gmail.com>
2020-10-31 20:42:45 -0300

    agregados inputs del main, instanciados buffer y threadPool

--- Tp-Concurrente/src/clases/Main.java
+++ Tp-Concurrente/src/clases/Main.java
@@ -1,8 +1,33 @@
 package clases;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Buffer buffer;
+    private static Scanner scanner;
+    private static ThreadPool threadPool;
+
     public static void main(String[] args) {
-	// write your code here
+
+        scanner = new Scanner(System.in);
+        long time = System.currentTimeMillis();
+
+        System.out.print("Indique la cantidad de Threads a utilizar: ");
+	    String threads = scanner.nextLine();
+
+	    System.out.print("Indique la dificultad a utilizar: ");
+        String dificultad = scanner.nextLine();
+
+        String texto = "";
+        System.out.print("Indique un texto: ");
+        texto = scanner.nextLine();
+
+
+        int cantWorkers = Integer.parseInt(threads);
+        buffer = new Buffer(cantWorkers);
+        threadPool = new ThreadPool(cantWorkers, buffer);
+
+
     }
 }
